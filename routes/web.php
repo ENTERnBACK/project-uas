da4df584bc47007b596a8c57af2c60d7e0537da3
Marchela Rembang <elizabethmarchela25@gmail.com>
2026-05-28 00:40:52 +0700

    Menambahkan route DriverLocation dan SupportTicket

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('driver-locations', DriverLocationController::class);
+
+Route::resource('support-tickets', SupportTicketController::class);
