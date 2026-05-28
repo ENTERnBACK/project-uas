3e411e13ead01398932761244de5268929bfbd53
earlymornn <marsyha619@gmail.com>
2026-05-28 13:35:01 +0700

    Update kode routes file web.php untuk Review.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DriverLocationController;
+use App\Http\Controllers\SupportTicketController;
+use App\Http\Controllers\ReviewController;
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/reviews');
 });
 
 Route::resource('driver-locations', DriverLocationController::class);
 
 Route::resource('support-tickets', SupportTicketController::class);
+
+Route::resource('reviews', ReviewController::class);
