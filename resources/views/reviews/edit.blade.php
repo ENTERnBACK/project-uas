653d2363c0595be8007fc8b8da024a2fabd77c21
earlymornn <marsyha619@gmail.com>
2026-05-28 16:27:38 +0700

    Update kode fitur CRUD di folder reviews untuk Review.

---
+++ resources/views/reviews/edit.blade.php
@@ -0,0 +1,16 @@
+<h1>Ubah Review</h1>
+<form method="POST" action="{{ route('reviews.update') }}">
+    @csrf
+    @method('PUT')
+    Rating:
+    <br>
+    <input type="number" name="rating" min="1" max="5" value="{{ $review->rating }}">
+    <br>
+    <br>
+    Ulasan:
+    <br>
+    <textarea name="review_driver" rows="8" required>{{ $review->review_driver }}</textarea>
+    <br>
+    <br>
+    <button type="submit">Simpan</button>
+</form>
---
+++ resources/views/reviews/show.blade.php
@@ -0,0 +1,5 @@
+<h1>{{ $review->rating }}</h1>
+
+<p>{{ $review->review_driver }}</p>
+
+<a href="{{ route('reviews.index') }}">Kembali</a>
