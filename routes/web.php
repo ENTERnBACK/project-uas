279c03cc83666c1b9617a113745d3ac4dca7fc16
graciacsonggigilan <graciacs.14052008@gmail.com>
2026-05-28 17:58:38 +0700

    Menambahkan kode Driver dan FavoriteLocation di file model,controller, dan view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\FavoriteLocationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('drivers', DriverController::class);
+
+Route::resource('favorite-locations', FavoriteLocationController::class);
