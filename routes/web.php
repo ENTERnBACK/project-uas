177902fa8398c11893130f4947fe769586510cb6
Marchela Rembang <elizabethmarchela25@gmail.com>
2026-05-28 22:28:22 +0700

    Menambahkan route untuk register, login, logout

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,26 @@ use App\Http\Controllers\FavoriteLocationController;
 use App\Http\Controllers\DriverLocationController;
 use App\Http\Controllers\SupportTicketController;
 use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return redirect('/reviews');
 });
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
+
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::resource('favorite-locations', FavoriteLocationController::class);
 
