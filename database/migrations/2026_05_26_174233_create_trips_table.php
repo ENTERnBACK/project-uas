0ef12fa0606752d983f56c320801c84fd298dcd8
earlymornn <marsyha619@gmail.com>
2026-06-10 15:07:43 +0700

    Menambahkan fitur Trips dan menambahkan kode di controllers, models, migrations, routes, serta di views

--- database/migrations/2026_05_26_174233_create_trips_table.php
+++ database/migrations/2026_05_26_174233_create_trips_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('trips', function (Blueprint $table) {
             $table->id();
+            $table->string('pickup_point');
+            $table->string('dropoff_point');
+            $table->enum('status', ['pending', 'on_trip', 'completed', 'cancelled'])->default('pending');
             $table->timestamps();
         });
     }
